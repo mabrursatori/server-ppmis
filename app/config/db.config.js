a67d84c6b0d3be2c50cfd42ce798b35ac092fa2d
mabrursatori <mabrursatori@gmail.com>
2021-07-28 15:03:27 +0700

    Change db

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,6 +1,13 @@
 module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "root",
-    DB: "ppmis"
+    HOST: "us-cdbr-east-04.cleardb.com",
+    USER: "bb5e15ded56120",
+    PASSWORD: "d3bd25d6",
+    DB: "heroku_702abcf05d178d5"
   };
+
+// module.exports = {
+//     HOST: "localhost",
+//     USER: "root",
+//     PASSWORD: "root",
+//     DB: "ppmis"
+//   };
