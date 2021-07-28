1df9d4df805ded2abb456be02296f21614d9e650
mabrursatori <mabrursatori@gmail.com>
2021-07-29 06:50:10 +0700

    revisi

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,9 +1,16 @@
 module.exports = {
-    HOST: process.env.DB_HOST,
-    USER: process.env.DB_USER,
-    PASSWORD: process.env.DB_PASS,
-    DB: process.env.DB_NAME
-  };
+  HOST: process.env.DB_HOST,
+  USER: process.env.DB_USER,
+  PASSWORD: process.env.DB_PASS,
+  DB: process.env.DB_NAME
+};
+
+// module.exports = {
+//     HOST: "us-cdbr-east-04.cleardb.com",
+//     USER: "bb5e15ded56120",
+//     PASSWORD: "bb5e15ded56120",
+//     DB: "heroku_702abcf05d178d5"
+//   };
 
 // module.exports = {
 //     HOST: "localhost",
