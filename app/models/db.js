1d30955cad7e9c25e54b962b97c2c3b7c06b0b64
mabrursatori <mabrursatori@gmail.com>
2021-07-29 10:59:39 +0700

    message

--- app/models/db.js
+++ app/models/db.js
@@ -2,18 +2,16 @@ const mysql = require("mysql2");
 const dbConfig = require("../config/db.config.js");
 
 // Create a connection to the database
-const connection = mysql.createConnection({
+const connection = mysql.createPool({
   host: dbConfig.HOST,
   user: dbConfig.USER,
   password: dbConfig.PASSWORD,
-  database: dbConfig.DB
+  database: dbConfig.DB,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
-// open the MySQL connection
-connection.connect(error => {
-  if (error) throw error;
-  console.log("Successfully connected to the database.");
-});
 
 //connection.end();
 
